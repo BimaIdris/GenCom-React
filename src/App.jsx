e84a2be98c9dd5ef7dde99899d1015f6ce1b965c
bimaidris <bimaidris@gmail.com>
2025-05-27 20:00:46 +0100

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Home from './components/Section1/home'
+import Home from './components/Section1/Home'
 import Section2 from './components/Section2/Section2'
 import Section3 from './components/Section3/Section3'
 
